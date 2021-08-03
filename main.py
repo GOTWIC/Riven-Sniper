fb6b072132bbefde660be28b528ce355eeac29fd
GOTWIC <swagnik17@gmail.com>
2021-08-02 20:29:37 -0400

    Support for unrolled rivens

--- main.py
+++ main.py
@@ -27,9 +27,10 @@ query = {"_id": mongoID}
 url = "https://api.warframe.market/v1/auctions?type=rivens"
 session = HTMLSession()
 client = commands.Bot(command_prefix='.')
-allRivenChannel = channel
+allRivensChannel = channel
 rawItemjson = session.get("https://api.warframe.market/v1/riven/items").json()
 items = rawItemjson['payload']['items']
+channels = [868140630491140137, 870140373723394058]
 
 
 cluster = MongoClient("mongodb+srv://GOTWIC:Swagnik10Caesar12_MED@cluster1.mu094.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE")
@@ -40,93 +41,76 @@ collection3 = db["Riven_Notification_List_Secondaries"]
 collection4 = db["Riven_Notification_List_Melees"]
 collection5 = db["Riven_Notification_List_Miscellaneous"]
 
+
 @client.event
 async def on_ready():
     print('\n\n|/---~---~---~---~---~---~---~---\|')
     print('| ~~ Logged in as ' + client.user.name + ' ~~ |')
     print('|\---~---~---~---~---~---~---~---/|\n\n')
-    await info(getALLChannel())
+    await info()
     buildNotificationCollections()
 
-async def getNewRivens(allRivenChannel):
+async def getNewRivens():
     while updateRivens:
         
         oldRivenIDs = queryMongoForOldRivens()
         rawDatajson = getRawData()
         auctions = rawDatajson['payload']['auctions']
-
         rawRivenIDs = []
-
         for id in auctions: 
            rawRivenIDs.append(id['id'])
-        
         newRivenIDs = [x for x in rawRivenIDs if x not in set(oldRivenIDs)]
-
         field = "Riven_IDs"
-  
         updateMongo(rawRivenIDs, field, collection1) 
-
         numberOfRivens = getArrSize(newRivenIDs)
 
-
-        if(numberOfRivens == 0):
-            embed = discord.Embed(
-                title = 'No new rivens have been created!',
-                #description = 'This is a description',
-                color = discord.Color.purple()
-            )
-            await allRivenChannel.send(embed=embed)
-
-        else:
+        if(numberOfRivens != 0):
             for item in range(numberOfRivens):
+
+                # Initialization of variables and arrays
+                # Get information from API and store info
+                # Create and return embed
+                # Use certain characteristics to find the right channel to post in
 
                 index = numberOfRivens - (item + 1)
                 auction = auctions[index]
-                riven = auction['item']
-                seller = auction['owner']
                 rivenStats = ["", "", "", ""]
                 statCount = 0
-
-                auctionUrl = "(https://warframe.market/auction/" + auction['id'] + ")"
+                auctionInfo = ""
 
                 if(auctions[index]['item']['type'] == "sister" or auctions[index]['item']['type'] == "lich"):
                     continue
 
-                slot = getItemAttribute(riven['weapon_url_name'], 'group')
+                riven = auction['item']
+                seller = auction['owner']
+                weaponName = riven['weapon_url_name']
+                rivenName = riven['name']
+                auctionURL = "(https://warframe.market/auction/" + auction['id'] + ")"
                 icon = getItemAttribute(riven['weapon_url_name'], 'icon')
-
-                embed = discord.Embed(
-                    title = riven['weapon_url_name'].replace("_", " ").title() + " " + riven['name'].title(),
-                    description = "Click [here]" + auctionUrl + " to go to auction",
-                    color = discord.Color.purple()
-                )
-
-                embed.set_thumbnail(url="https://warframe.market/static/assets/" + icon)
-
-                for stat in riven['attributes']:
-                    rawStat = stat['url_name'].replace("_", " ").title()
-                    result = abbreviateStat(rawStat, slot)  + ": " + str(stat['value'])
-                    rivenStats[statCount] = result
-                    statCount += 1
-                embed.add_field(name='Stats', value= rivenStats[0] + '\n' + rivenStats[1] + '\n' + rivenStats[2] + '\n' + rivenStats[3], inline=True)
-            
+                slot = getItemAttribute(riven['weapon_url_name'], 'group')
+                isDirectSell = auction['is_direct_sell']
                 buyout = str(auction['buyout_price'])
                 topBid = str(auction['top_bid'])
+                rolls = riven['re_rolls']
+
                 if(buyout == "None"):
                     buyout = "∞"
                 if(topBid == "None"):
                     topBid = "0"
-                if(auction['is_direct_sell'] == True):
-                    embed.add_field(name='Offer', value='Sell Price: ' + str(auction['starting_price']), inline=True)
+                if(isDirectSell == True):
+                    auctionInfo = 'Sell Price: ' + str(auction['starting_price'])
                 else:
-                    embed.add_field(name='Offer', value='Starting Price: ' + str(auction['starting_price']) + "\n Buyout Price: " + buyout + "\n Top Bid: " + topBid, inline=True)
-            
-                embed.add_field(name='Seller', value=seller['ingame_name'] + '\n' + seller['status'].title(), inline=True)
+                    auctionInfo = 'Starting Price: ' + str(auction['starting_price']) + "\n Buyout Price: " + buyout + "\n Top Bid: " + topBid
+                for stat in riven['attributes']:
+                    rawStat = stat['url_name'].replace("_", " ").title()
+                    rivenStats[statCount] = abbreviateStat(rawStat, slot)  + ": " + str(stat['value'])
+                    statCount += 1
 
-                #embed.add_field(name='Auction Link', value="Click [here]" + auctionUrl + " to go to auction", inline=True)
+                rivenEmbed = createRivenEmbed(weaponName, rivenName, auctionURL, seller, icon, auctionInfo, rivenStats)
 
+                await sendRivenEmbed(rivenEmbed, rolls)
 
-                await allRivenChannel.send(embed=embed)
+                
 
         await asyncio.sleep(30)
 
@@ -175,8 +159,26 @@ async def sendSimpleEmbed(string, channel):
             )
     await channel.send(embed=embed)
    
-def getALLChannel():
-    return client.get_channel(868140630491140137)
+async def sendRivenEmbed(embed, rolls):
+    await client.get_channel(channels[0]).send(embed=embed)
+
+    if(rolls == 0):
+        await client.get_channel(channels[1]).send(embed=embed)
+
+   
+
+def createRivenEmbed(weaponName, rivenName, auctionURL, seller, thumbnail, auctionInfo, rivenStats):
+    embed = discord.Embed(
+        title = weaponName.replace("_", " ").title() + " " + rivenName.title(),
+        description = "Click [here]" + auctionURL + " to go to auction",
+        color = discord.Color.purple()
+        )
+    embed.add_field(name='Stats', value= rivenStats[0] + '\n' + rivenStats[1] + '\n' + rivenStats[2] + '\n' + rivenStats[3], inline=True)
+    embed.add_field(name='Offer', value=auctionInfo, inline=True)
+    embed.add_field(name='Seller', value=seller['ingame_name'] + '\n' + seller['status'].title(), inline=True)
+    embed.set_thumbnail(url="https://warframe.market/static/assets/" + thumbnail)
+
+    return embed
 
 def queryMongoForOldRivens():
     if (collection1.count_documents(query) == 0):
@@ -326,8 +328,8 @@ def getRawData():
     return rawDatajson
 
 @client.command()
-async def info(channel):
-    client.loop.create_task(getNewRivens(channel))
+async def info():
+    client.loop.create_task(getNewRivens())
 
 
 client.run('ODY4MTM3NjQyMzA5NTgyODU4.YPrSLw.ewZd22TCBkDTDMN-__QxwjhZ9uM')
